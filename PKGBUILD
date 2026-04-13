5383704c15f9269846f504432d1676872d3791cc
CodeKing710 <cjrox01@yahoo.com>
2026-04-13 15:47:31 +0000

    NFF baseline is ready to go with defaults and such. PKGBUILD is working and SRCINFO and install files and scripts were added to ensure a smooth install experience. Ruleset parsing is in progress as well as pretty printed rulesets

--- PKGBUILD
+++ PKGBUILD
@@ -1,14 +1,33 @@
 pkgname=nff
 pkgver=0.1.0
 pkgrel=1
+pkgdesc="An nftables firewall tool built in Python. Not as robust as direct nftables usage but nice to load static rules similar to ufw"
 arch=('any')
 depends=('python' 'python-yaml' 'nftables')
-source=('nff.py' 'nff-defaults.yml')
-sha256sums=('SKIP' 'SKIP')
+backup=('etc/nff.yml' 'etc/nftables.d/00-nff.nft')
+install=nff.install
+source=('nff.py' 'nff-defaults.yml' '00-nff.nft' 'help' 'version')
+sha256sums=('SKIP' 'SKIP' 'SKIP' 'SKIP' 'SKIP')
 
 package() {
+  # Create necessary directories
+  install -d "$pkgdir/usr/share/nff"
+  install -d "$pkgdir/usr/bin"
+  install -d "$pkgdir/etc/nff.d"
+  install -d "$pkgdir/etc/nftables.d"
+
+  # Install main executable
   install -Dm755 nff.py "$pkgdir/usr/bin/nff"
-  install -Dm644 nff.yml "$pkgdir/etc/nff.yml"
-  mkdir -p "$pkgdir/etc/nff.d"
-  chmod 755 "$pkgdir/etc/nff.d"
+
+  # Install configs
+  install -Dm644 nff-defaults.yml "$pkgdir/etc/nff.yml"
+  install -Dm644 00-nff.nft "$pkgdir/etc/nftables.d/00-nff.nft"
+
+  # Backup defaults to the share
+  install -Dm644 nff-defaults.yml "$pkgdir/usr/share/nff/nff-defaults.yml"
+  install -Dm644 00-nff.nft "$pkgdir/usr/share/nff/00-nff.nft"
+
+  # Install Misc data
+  install -Dm644 help "$pkgdir/usr/share/nff/help"
+  install -Dm644 version "$pkgdir/usr/share/nff/version"
 }
--- nff.py
+++ nff.py
@@ -1,15 +1,28 @@
-#!/bin/env python
+#!/usr/bin/env python
 
 import argparse
 import yaml
 import sys
+import subprocess
 from pathlib import Path
 
 MAIN_CONFIG = Path("/etc/nff.yml")
 INCLUDED_CONFIG = Path("/etc/nff.d")
+SHARE_PATH = Path("/usr/share/nff")
+NFT_CONFIG = Path("/etc/nftables.d/00-nff.nft")
+VERBOSE = False
+
+# Verbose logging function
+def verbose(*msgs):
+    if VERBOSE:
+        print(*msgs)
+
 
 # Configuration functions
 def load_file(path):
+    if not path.exists():
+        print(f"Error finding file {path}", file=sys.stderr)
+        sys.exit(1)
     try:
         with open(path, "r") as f:
             return yaml.safe_load(f) or {}
@@ -17,26 +30,75 @@ def load_file(path):
         print(f"Error loading file {path}: {e}", file=sys.stderr)
         sys.exit(1)
 
-def load_cfg(path):
-    pass
+def load_cfg():
+    config = load_file(MAIN_CONFIG)
+    if INCLUDED_CONFIG.is_dir():
+        for extra in INCLUDED_CONFIG.glob("*.yml"):
+            config.update(load_file(extra))
+    return config
 
-def merge_cfgs(base, override):
-    pass
+# Singletons
+def show_version():
+    with open(SHARE_PATH / "version", 'r') as f:
+        print(f.read().strip())
 
 # Execution flag functions
-def show_rules():
-    pass
+def show_rules(args, config):
+    if args.nfstyle:
+        try:
+            result = subprocess.run(["nft","list","ruleset"], capture_output=True, text=True)
+            print(result.stdout)
+        except FileNotFoundError:
+            print("Error: nftables is not accessible. Check if it is installed and in the PATH", file=sys.stderr)
+    else:
+        # Print out the YAML config in a nice way
+        print(config)
 
-def apply_rules():
-    pass
+def apply_rules(args, config):
+    if NFT_CONFIG.exists():
+        verbose(f"Applying NFF rules...")
+        subprocess.run(["nft","-f",str(NFT_CONFIG)], check=True)
+
+    print("NFF rules applied!")
 
 def arg_parser():
-    pass
+    parser = argparse.ArgumentParser(description="nff: A simple nftables firewall tool")
+    parser.add_argument('-V', '--verbose', action='store_true')
+    parser.add_argument('-v', '--version', action='store_true')
+    subparsers = parser.add_subparsers(dest="status | apply")
+
+    # Show rules
+
+    show_p = subparsers.add_parser('status')
+    show_p.add_argument('-N', '--nfstyle', action='store_true')
+    show_p.set_defaults(func=show_rules)
+
+    # Apply Configs
+
+    apply_p = subparsers.add_parser('apply')
+    apply_p.set_defaults(func=apply_rules)
+
+    return parser
 
 def main():
+    global VERBOSE
     parser = arg_parser()
     args = parser.parse_args()
-    args.func(args)
+
+    # Check verbosity
+    if args.verbose:
+        VERBOSE = True
+    elif args.version:
+        show_version()
+        sys.exit(0)
+
+    # Load configs and begin
+    config = load_cfg()
+    if hasattr(args, 'func'):
+        args.func(args, config)
+    else:
+        with open(SHARE_PATH / "help",'r') as f:
+            print(f.read())
 
 if __name__ == "__main__":
     main()
